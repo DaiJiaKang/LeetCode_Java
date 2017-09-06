1fb9effe55bfe0ddb5dccd3ce903ecb668519cef
xiaodai <543490673@qq.com>
2017-09-06 11:34:37 +0800

    xiaodai

---
+++ src/XiaoDai/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,39 @@
+package XiaoDai;
+
+public class RemoveNthNodeFromEndofList {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { 
+    	val = x; 
+    }
+}
+
+class Solution {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+    	if(head == null)		return head;
+        int m = 1;
+        ListNode pre = head, next = head, cur = null;
+        while(m < n) {
+        	next = next.next;
+        	m++;
+        }
+        if(next.next == null)	return head.next;
+        while(next.next != null) {
+        	next = next.next;
+        	cur = pre;
+        	pre = pre.next;
+        }
+        if(pre == next)			cur.next = null;
+        else					cur.next = pre.next;
+        return head;
+    }
+}
