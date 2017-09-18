dad5b600ecbdbc12fe00adfc7e442a6936428380
xiaodai <543490673@qq.com>
2017-09-18 17:39:48 +0800

    xiaodai

---
+++ src/XiaoDai/NextPermutation.java
@@ -0,0 +1,46 @@
+package XiaoDai;
+
+public class NextPermutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = {1, 3, 2};
+		SolutionNextPermutation.nextPermutation(nums);
+		for(int num : nums) {
+			System.out.println(num);
+		}
+	}
+
+}
+
+class SolutionNextPermutation {
+    public static void nextPermutation(int[] nums) {
+    	if(nums.length < 2)
+    		return;
+        int start = nums.length - 2;
+        while(start >= 0) {
+        	if(nums[start] < nums[start + 1]) {
+        		int tmp = 0, i = 0;
+        		for(i = nums.length - 1; i >= start + 1; --i) {
+        			if(nums[i] > nums[start]) {
+        				break;
+        			}
+        		}
+        		tmp = nums[start];
+        		nums[start] = nums[i];
+        		nums[i] = tmp;
+        		
+        		int end = nums.length - 1;
+            	start = start + 1;
+            	while(start < end) {
+            		tmp = nums[start];
+            		nums[start] = nums[end];
+            		nums[end] = tmp;
+            		start++; end--;
+            	}
+            	break;
+        	}
+        	start--;
+        }
+    }
+}
